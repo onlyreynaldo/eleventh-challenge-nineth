ae00f232010c8d1aec6ab1ecc0b890e667ade8c9
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-07-15 19:58:56 -0300

    feat(main): add create new order

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -3,6 +3,7 @@ import { getRepository, Repository, In } from 'typeorm';
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
 import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
+import AppError from '@shared/errors/AppError';
 import Product from '../entities/Product';
 
 interface IFindProducts {
@@ -43,13 +44,45 @@ class ProductsRepository implements IProductsRepository {
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const idList = products.map(product => product.id);
+    const orderList = await this.ormRepository.find({
+      where: { id: In(idList) },
+    });
+
+    if (idList.length !== orderList.length) {
+      throw new AppError('Missing product');
+    }
+
+    return orderList;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productsData = await this.findAllById(products);
+    const newProducts = productsData.map(productData => {
+      const productFind = products.find(
+        product => product.id === productData.id,
+      );
+
+      if (!productFind) {
+        throw new AppError('Product not find');
+      }
+
+      if (productData.quantity < productFind.quantity) {
+        throw new AppError('Insuficient product quantity');
+      }
+
+      const newProduct = productData;
+
+      newProduct.quantity -= productFind.quantity;
+
+      return newProduct;
+    });
+
+    await this.ormRepository.save(newProducts);
+
+    return newProducts;
   }
 }
 
